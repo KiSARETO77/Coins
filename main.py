45b99425904a15902051d149f3c26046a043f5a7
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-10-26 16:45:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep as ZZ
 from os import system as Z
 Ç=Ç()
 Ç.login("au-@digdig.org","GOKU12")
-while 0:
+while 1:
 	for X in range(24):
 		Ç.send_active(64112096)
 		print(f"{X + 1} - Send Time")
